6e2eb61ec593689452e815d6bc833604259ef6ab
Sapphire-Creativity <sapphirecreativity@gmail.com>
2023-10-07 11:03:02 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the alphabet in lowercase
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			if ((c != 'q' && c != 'c') && c <= 'z')
+				putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
