a03dbdc24879395186904edc39c95a766e38dcf9
Sapphire-Creativity <sapphirecreativity@gmail.com>
2023-10-11 19:24:09 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,7 +12,7 @@ void times_table(void)
 
 	for (x = 0; x <= 9; x++)
 	{
-	for (u = 0; y <= 9; y++)
+	for (y = 0; y <= 9; y++)
 	{
 	z = x * y;
 	if (z > 9)
@@ -20,7 +20,7 @@ void times_table(void)
 	u = z % 10;
 	d = (z - u) / 10;
 	_putchar(44);
-	_putchar(32);
+	_putcha(32);
 	_putchar(d + '0');
 	_putchar(u + '0');
 	}
@@ -28,7 +28,7 @@ void times_table(void)
 	{
 	if (y != 0)
 	{
-	_putchar (44);
+	_putchar(44);
 	_putchar(32);
 	_putchar(32);
 	}
@@ -38,6 +38,3 @@ void times_table(void)
 	_putchar('\n');
 	}
 }
-
-
-	
