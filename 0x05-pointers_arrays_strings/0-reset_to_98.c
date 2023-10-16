85c58e6756bd2c491cd287f504fd84868b55a7e4
Sapphire-Creativity <sapphirecreativity@gmail.com>
2023-10-16 11:16:15 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reset_to_98 - a - a function that takes a pointer to an int as parameter 
+ * reset_to_98 - a - a function that takes a pointer to an int as parameter
  * @n: input
  * Return: n
  */
