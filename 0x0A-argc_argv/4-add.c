4f9f70bec7a11e1a7a06a12cc3ff854166d7cb23
Sapphire-Creativity <sapphirecreativity@gmail.com>
2023-11-01 14:23:35 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
 			e = argv[i];
 		for (k = 0; k < strlen(e); k++)
 		{
-			if (e[k] < 48 || > e[k] > 57)
+			if (e[k] < 48 || e[k] > 57)
 			{
 				printf("Error\n");
 				return (1);
