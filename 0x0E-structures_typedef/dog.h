d769d6caa2fe390e10eb5fb49a852b78fa5eec2a
Sapphire-Creativity <sapphirecreativity@gmail.com>
2023-11-06 06:42:42 -0800

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,7 +20,7 @@ void print_dog(struct dog *d);
 typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *strcpy(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 
 #endif
